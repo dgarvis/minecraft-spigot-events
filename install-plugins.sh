41ca9a4d01e528548cb234c5616b18d5592d24d7
Dylan Garvis <dylan@garvis.dev>
2023-11-22 17:29:21 -0500

    fixed bugs in the config. plugins now load

---
+++ install-plugins.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp mces-spigot/target/mces-spigot-1.0-SNAPSHOT.jar test-env/world1-plugins/
+cp mces-spigot/target/mces-spigot-1.0-SNAPSHOT.jar test-env/world2-plugins/
+
+cp mces-bungee/target/mces-bungee-1.0-SNAPSHOT.jar test-env/bungee-plugins
