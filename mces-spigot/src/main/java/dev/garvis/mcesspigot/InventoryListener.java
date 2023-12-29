7bfe0636275a70be1796e4f24149f5aa88ff06e6
Dylan Garvis <dylan@garvis.dev>
2023-12-29 16:42:51 -0500

    Fixed crafting amount calculation.

--- mces-spigot/src/main/java/dev/garvis/mcesspigot/InventoryListener.java
+++ mces-spigot/src/main/java/dev/garvis/mcesspigot/InventoryListener.java
@@ -3,10 +3,13 @@ package dev.garvis.mcesspigot;
 import dev.garvis.mcesspigot.KafkaManager;
 
 import org.bukkit.Bukkit;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.inventory.ClickType;
 
 import java.util.Map;
 import java.util.HashMap;
@@ -36,7 +39,25 @@ public class InventoryListener implements Listener {
 	e.put("server", serverName);
 
 	e.put("item", event.getRecipe().getResult().getType().toString());
-	e.put("amount", event.getRecipe().getResult().getAmount());
+
+	// Let's calculate the amount of it crafted.
+	// https://www.spigotmc.org/threads/how-to-get-amount-of-item-crafted.377598/#post-3896072
+	ItemStack craftedItem = event.getInventory().getResult();
+	Inventory inventory = event.getInventory();
+	ClickType clickType = event.getClick();
+	int realAmount = craftedItem.getAmount();
+	if (clickType.isShiftClick()) {
+	    int lowerAmount = craftedItem.getMaxStackSize() + 1000;
+	    for (ItemStack actualItem : inventory.getContents()){
+		// If slot is not air and lower amount is higher than this slot amonut and it's not the reipe amount
+		if (!actualItem.getType().isAir() && lowerAmount > actualItem.getAmount() && !actualItem.getType().equals(craftedItem.getType())) {
+		    lowerAmount = actualItem.getAmount();
+		}		
+	    }
+	    realAmount = lowerAmount * craftedItem.getAmount();
+	}
+	
+	e.put("amount", realAmount);
 
 	kafka.sendMessage(e);
     }
